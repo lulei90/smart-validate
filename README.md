9c7a77e34ab8517c2be3bbbcf1e06578d218537a
卢云俊 <luyj@yqjr.com.cn>
2018-05-10 13:46:29 +0800

    修复部分验证失效问题

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # redux-form-validating
-Simple syncValidate with redux-form
+[![codebeat badge](https://codebeat.co/badges/16727cff-eabe-4eed-91e4-2bb8c1832a3b)](https://codebeat.co/projects/github-com-lulei90-redux-form-validating-master)
+
+**Simple syncValidate with redux-form 基于redux-form的同步表单验证**
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import { defaultRules,defaultErrorTip,defaultNullTip } from './defaultOptions'
 /***
  *
- * @param scheme 需要验证的计划
+ * @param schema 需要验证的模型
  * @param rules 自定义的验证规则
  * @param errorTip 自定义的错误提示
  * @param nullTip 自定义的为空提示
  * @returns {Function} 返回一个加工好的验证方法 接受values对象用之前配置的验证计划来验证
  */
-function validate({scheme={},rules={},errorTip={},nullTip={}}){
+function validate({schema={},rules={},errorTip={},nullTip={}}){
   const _rules=Object.assign({},defaultRules,rules);
   const _errorTip=Object.assign({},defaultErrorTip,errorTip);
   const _nullTip=Object.assign({},defaultNullTip,nullTip);
   function check(rule,key,values,error){
-    const ruleType = _rules[rule] || ((typeof rule === 'function')? rule:/[\s\S]+/);
+    const ruleType = _rules[rule] || rule;
     const _check = (ruleType.test && ruleType.test.bind(ruleType)) || ruleType;
     let flag = _check(values[key],values,key);
     if(flag === true) return true;
@@ -22,14 +22,18 @@ function validate({scheme={},rules={},errorTip={},nullTip={}}){
   }
   return function(values){
     let error = {};
-    for(const key in scheme){
-      if(values[key] === undefined && (typeof rules !=='function') && !(rules.indexOf('ignore')!== -1)){
-        error[key] = _nullTip[key] || _nullTip.def ;
+    for(const key in schema){
+      const rules =schema[key];
+      const type = Object.prototype.toString.call(rules);
+      if(values[key] === undefined){
+        if(type !=='[object Array]' || rules.indexOf('ignore') === -1){
+          error[key] = _nullTip[key] || _nullTip.def ;
+        }
         continue
       }
-      const rules =scheme[key];
-      switch (Object.prototype.toString.call(rules)){
+      switch (type){
         case '[object String]':
+        case '[object RegExp]':
         case '[object Function]':
           check(rules,key,values,error);
           break;
@@ -37,12 +41,12 @@ function validate({scheme={},rules={},errorTip={},nullTip={}}){
           rules.every((rule)=>check(rule,key,values,error));
           break;
         default:
-          break;
+        console.error('暂不支持此类型规则');
       }
-      let errorArry = Object.values(error);
-      if(errorArry.length>0){
-        error._error = errorArry[0];
-      }
+    }
+    let errorArry = Object.values(error);
+    if(errorArry.length>0){
+      error._error = errorArry[0];
     }
     return  error;
   }
